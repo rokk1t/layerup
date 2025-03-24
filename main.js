0ecc1bd46f043aa0bec37d97b8cc44bd60e048f7
Viktor Rud <99555918+rokk1t@users.noreply.github.com>
2025-03-24 21:53:47 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+
+import React from "https://esm.sh/react";
+import ReactDOM from "https://esm.sh/react-dom";
+function App() {
+  return React.createElement('div', {}, 'Layer UP');
+}
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
